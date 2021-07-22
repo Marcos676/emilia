161c5cf79562e4922b559d86f3e95cce304fbd8a
Gabriel <patino-marcos@outlook.com>
2021-07-22 13:26:26 -0300

    vista about

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = 3000
 app.use(express.static('public'));
 
 app.get( '/' , (req , res) => res.sendFile(path.join(__dirname, 'views', 'home.html'))  )
+app.get( '/about' , (req , res) => res.sendFile(path.join(__dirname, 'views', 'home.html'))  )
 
 
 app.listen( port , () => console.log('Servidor corriendo en el puerto ' + port + ' http://localhost:' + port))
