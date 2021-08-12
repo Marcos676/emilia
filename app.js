f156cd213da1702e7342cadc5f534d25f54e530f
Gabriel <patino-marcos@outlook.com>
2021-08-12 12:45:56 -0300

    crea vista music

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static('public'));
 
 app.get( '/' , (req , res) => res.sendFile(path.join(__dirname, 'views', 'home.html'))  )
 app.get( '/about' , (req , res) => res.sendFile(path.join(__dirname, 'views', 'home.html'))  )
+app.get( '/music' , (req , res) => res.sendFile(path.join(__dirname, 'views', 'music.html'))  )
 
 
 app.listen( port , () => console.log('Servidor corriendo en el puerto ' + port + ' http://localhost:' + port))
